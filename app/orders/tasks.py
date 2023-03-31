817f3d681e43293f4ca39e4ce4e20c0e9f2cee80
Dmitrii Shmidt <shmicer@gmail.com>
2023-03-31 09:21:28 +0300

    add celery worker, flower and celery-beat to async email tasks

--- app/orders/tasks.py
+++ app/orders/tasks.py
@@ -4,9 +4,9 @@ from celery import shared_task
 @shared_task()
 def send_order_email_task(email_address, order):
     send_mail(
-        f'Your order {order}',
-        f'\n\nThank you!',
-        'support@example.com',
+        "Your Order",
+        f"Your order {order}",
+        "support@example.com",
         [email_address],
         fail_silently=False,
     )
