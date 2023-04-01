ed166b51bee5db430e1e112b2a7186a79d142c37
Dmitrii Shmidt <shmicer@gmail.com>
2023-04-01 21:49:49 +0300

    adding celery and celery-beat for schedule email messages

--- app/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('checkout', views.checkout, name='checkout'),
-    path('create/', views.create_order, name='order_create'),
-]
