bcd2c46204ed9a476aa130eb8c90c55bca9d5fbc
Dmitrii Shmidt <shmicer@gmail.com>
2023-04-04 12:06:34 +0300

    Revert "add celery worker, flower and celery-beat to async email tasks"
    This reverts commit e4165a55

--- app/config/celery_app.py
+++ app/config/celery_app.py
@@ -1,16 +1,18 @@
 import os
-
 from celery import Celery
 from celery.schedules import crontab
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+
 app.conf.beat_schedule = {
-    'send_subscription_mail_every_5_minutes': {
-        'task': 'core.tasks.send_subscription_mail_task',
+    'send_spam_every_5_min': {
+        'task': 'account.tasks.send_beat_email',
         'schedule': crontab(minute='*/5'),
     },
 }
